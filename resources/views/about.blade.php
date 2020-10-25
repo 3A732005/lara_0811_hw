633cb97ee2fb42afa3d37a58d6d5a05b6494bd33
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-25 16:02:36 +0800

    about.index相關操作

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <title>about</title>
+</head>
+
+<body>
+<a href="{{route('about.index')}}">about</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+
+
